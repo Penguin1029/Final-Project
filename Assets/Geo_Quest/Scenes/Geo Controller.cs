46d04e6db6fca2521c79e39f4fc98b41df269870
Penguin1029 <Jgarpe1029@gmail.com>
2025-03-15 10:02:55 -0400

    yippe

--- Assets/Geo_Quest/Scenes/Geo Controller.cs
+++ Assets/Geo_Quest/Scenes/Geo Controller.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 public class GeoController : MonoBehaviour
 {
 
-
+    SpriteRenderer spriteRenderer;
 
     Rigidbody2D rb;
     int speed = 5;
@@ -17,34 +17,41 @@ public class GeoController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-
-        //if(Input.GetKeyDown(KeyCode.W))
-        //transform.position += new Vector3(0, 1, 0);
-        //if (Input.GetKeyDown(KeyCode.A))
-        // transform.position += new Vector3(-1, 0, 0);
-        //if (Input.GetKeyDown(KeyCode.S))
-        // transform.position += new Vector3(0, -1, 0);
-        //  if (Input.GetKeyDown(KeyCode.D))
-        //  transform.position += new Vector3(1, 0, 0);
-
-        // if (Input.GetKey(KeyCode.A))
-        // rb.velocity = new Vector2 (-1, rb.velocity.y);
-        // if (Input.GetKey(KeyCode.D))
-        //  rb.velocity = new  Vector2 (1,rb.velocity.y);
+        
+        
+        
+        
 
         float xinput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xinput, rb.velocity.y);
 
         rb.velocity = new Vector2(xinput * speed, rb.velocity.y);
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            spriteRenderer.color = Color.yellow;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            spriteRenderer.color = Color.blue;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            spriteRenderer.color = Color.red;
+
+        }
 
     }
 
